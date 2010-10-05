b8a9d31f5b5754a38029e68721b313a04112d5fd
cde <cdechauri@gmail.com>
2010-10-05 07:17:13 -0700

    Commit del esqueleto rails 3.0 con ruby 1.8. Error con Ruby 1.9 :(

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jugando::Application.config.secret_token = '690dbd4fd0ab5cb67614c920893271fcda19b5da15830a2f8c389c907aa3212daba0ebd3661930662942a2015f0042c312a27bc15bd6ac52af53334b9cf6de94'
